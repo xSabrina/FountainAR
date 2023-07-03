93af09bad173c747eb4e34260cb153f35c0df2bd
Sabrina Freisleben <41908872+xSabrina@users.noreply.github.com>
2023-07-03 14:42:21 +0200

    Formatting

--- app/src/main/java/com/example/fountainar/helpers/CameraPermissionHelper.java
+++ app/src/main/java/com/example/fountainar/helpers/CameraPermissionHelper.java
@@ -28,38 +28,40 @@ import androidx.core.app.ActivityCompat;
  * Helper to ask for camera permission.
  */
 public final class CameraPermissionHelper {
-  private static final int CAMERA_PERMISSION_CODE = 2;
-  private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
+    private static final int CAMERA_PERMISSION_CODE = 2;
+    private static final String CAMERA_PERMISSION = Manifest.permission.CAMERA;
 
-  /**
-   * Checks, if we have the necessary permissions for this app.
-   */
-  public static boolean hasNoCameraPermission(Activity activity) {
-    return ActivityCompat.checkSelfPermission(activity, CAMERA_PERMISSION)
-            != PackageManager.PERMISSION_GRANTED;
-  }
+    /**
+     * Check, if we have the necessary permissions for this app.
+     */
+    public static boolean hasNoCameraPermission(Activity activity) {
+        return ActivityCompat.checkSelfPermission(activity, CAMERA_PERMISSION)
+                != PackageManager.PERMISSION_GRANTED;
+    }
 
-  /**
-   * Asks for the camera permission.
-   */
-  public static void requestCameraPermission(Activity activity) {
-    ActivityCompat.requestPermissions(
-            activity, new String[]{CAMERA_PERMISSION}, CAMERA_PERMISSION_CODE);
-  }
+    /**
+     * Ask for the camera permission.
+     */
+    public static void requestCameraPermission(Activity activity) {
+        ActivityCompat.requestPermissions(
+                activity, new String[]{CAMERA_PERMISSION}, CAMERA_PERMISSION_CODE);
+    }
 
-  /**
-   * Checks, if we need to show the rationale for this permission.
-   */
-  public static boolean shouldShowRequestPermissionRationale(Activity activity) {
-    return ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION);
-  }
+    /**
+     * Check, if we need to show the rationale for this permission.
+     */
+    public static boolean shouldShowRequestPermissionRationale(Activity activity) {
+        return ActivityCompat.shouldShowRequestPermissionRationale(activity, CAMERA_PERMISSION);
+    }
 
-  /** Launch application settings to grant permission. */
-  public static void launchPermissionSettings(Activity activity) {
-    Intent intent = new Intent();
-    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-    intent.setData(Uri.fromParts("package", activity.getPackageName(), null));
-    activity.startActivity(intent);
-  }
+    /**
+     * Launch application settings to grant permission.
+     */
+    public static void launchPermissionSettings(Activity activity) {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.fromParts("package", activity.getPackageName(), null));
+        activity.startActivity(intent);
+    }
 
 }
--- app/src/main/java/com/example/fountainar/helpers/DisplayRotationHelper.java
+++ app/src/main/java/com/example/fountainar/helpers/DisplayRotationHelper.java
@@ -29,79 +29,79 @@ import com.google.ar.core.Session;
  * events.
  */
 public final class DisplayRotationHelper implements DisplayListener {
-  private final Display display;
-  private final DisplayManager displayManager;
-  private boolean viewportChanged;
-  private int viewportWidth;
-  private int viewportHeight;
+    private final Display display;
+    private final DisplayManager displayManager;
+    private boolean viewportChanged;
+    private int viewportWidth;
+    private int viewportHeight;
 
-  /**
-   * Constructs the DisplayRotationHelper but does not register the listener yet.
-   *
-   * @param context the Android {@link Context}.
-   */
-  public DisplayRotationHelper(Context context) {
-    displayManager = (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
+    /**
+     * Construct the DisplayRotationHelper but does not register the listener yet.
+     *
+     * @param context the Android {@link Context}.
+     */
+    public DisplayRotationHelper(Context context) {
+        displayManager = (DisplayManager) context.getSystemService(Context.DISPLAY_SERVICE);
 
-    WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-    display = windowManager.getDefaultDisplay();
-  }
-
-  /**
-   * Registers the display listener. Should be called from onResume().
-   */
-  public void onResume() {
-    displayManager.registerDisplayListener(this, null);
-  }
-
-  /**
-   * Unregisters the display listener. Should be called from onPause().
-   */
-  public void onPause() {
-    displayManager.unregisterDisplayListener(this);
-  }
-
-  /**
-   * Records a change in surface dimensions. This will be later used by {@link
-   * #updateSessionIfNeeded(Session)}. Should be called from {@link
-   * android.opengl.GLSurfaceView.Renderer
-   * #onSurfaceChanged(javax.microedition.khronos.opengles.GL10, int, int)}.
-   *
-   * @param width  the updated width of the surface.
-   * @param height the updated height of the surface.
-   */
-  public void onSurfaceChanged(int width, int height) {
-    viewportWidth = width;
-    viewportHeight = height;
-    viewportChanged = true;
-  }
-
-  /**
-   * Updates the session display geometry if a change was posted either by {@link
-   * #onSurfaceChanged(int, int)} call or by {@link #onDisplayChanged(int)} system callback. This
-   * function should be called explicitly before each call to {@link Session#update()}. This
-   * function will also clear the 'pending update' (viewportChanged) flag.
-   *
-   * @param session the {@link Session} object to update if display geometry changed.
-   */
-  public void updateSessionIfNeeded(Session session) {
-    if (viewportChanged) {
-      int displayRotation = display.getRotation();
-      session.setDisplayGeometry(displayRotation, viewportWidth, viewportHeight);
-      viewportChanged = false;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        display = windowManager.getDefaultDisplay();
     }
-  }
 
-  @Override
-  public void onDisplayAdded(int displayId) {
-  }
+    /**
+     * Register the display listener. Should be called from onResume().
+     */
+    public void onResume() {
+        displayManager.registerDisplayListener(this, null);
+    }
 
-  @Override
-  public void onDisplayRemoved(int displayId) {
-  }
+    /**
+     * Unregister the display listener. Should be called from onPause().
+     */
+    public void onPause() {
+        displayManager.unregisterDisplayListener(this);
+    }
 
-  @Override
-  public void onDisplayChanged(int displayId) {
-    viewportChanged = true;
-  }
+    /**
+     * Record a change in surface dimensions. This will be later used by {@link
+     * #updateSessionIfNeeded(Session)}. Should be called from {@link
+     * android.opengl.GLSurfaceView.Renderer
+     * #onSurfaceChanged(javax.microedition.khronos.opengles.GL10, int, int)}.
+     *
+     * @param width  the updated width of the surface.
+     * @param height the updated height of the surface.
+     */
+    public void onSurfaceChanged(int width, int height) {
+        viewportWidth = width;
+        viewportHeight = height;
+        viewportChanged = true;
+    }
+
+    /**
+     * Update the session display geometry if a change was posted either by {@link
+     * #onSurfaceChanged(int, int)} call or by {@link #onDisplayChanged(int)} system callback. This
+     * function should be called explicitly before each call to {@link Session#update()}. This
+     * function will also clear the 'pending update' (viewportChanged) flag.
+     *
+     * @param session the {@link Session} object to update if display geometry changed.
+     */
+    public void updateSessionIfNeeded(Session session) {
+        if (viewportChanged) {
+            int displayRotation = display.getRotation();
+            session.setDisplayGeometry(displayRotation, viewportWidth, viewportHeight);
+            viewportChanged = false;
+        }
+    }
+
+    @Override
+    public void onDisplayAdded(int displayId) {
+    }
+
+    @Override
+    public void onDisplayRemoved(int displayId) {
+    }
+
+    @Override
+    public void onDisplayChanged(int displayId) {
+        viewportChanged = true;
+    }
 }
